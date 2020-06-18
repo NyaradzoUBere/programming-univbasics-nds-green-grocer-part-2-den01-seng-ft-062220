e473e945dc9f32078079e58f392b0a46fb38d0e5
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-18 16:42:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,7 +6,9 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_collection(coupons[counter][:item], cart)
     couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_collection (couponed_item_name, cart)
-    if cart_item && 
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon
       
     end 
     counter += 1
