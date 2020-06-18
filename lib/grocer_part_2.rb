3b8c6d9141874e43544e52426cdd171450170537
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-18 16:37:00 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,40 +1,8 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-    coupons.each do |coupon| 
-    coupon.each do |key, value| 
-      name = coupon[:item] 
-    
-      if cart[name] && cart[name][:count] >= coupon[:num] 
-        if cart["#{name} W/COUPON"] 
-          cart["#{name} W/COUPON"][:count] += 1 
-        else 
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
-          :clearance => cart[name][:clearance], :count => 1} 
-        end 
+
   
-      cart[name][:count] -= coupon[:num] 
-    end 
-  end 
-end 
-  cart 
-end
-
-
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-  cart.each do |item, key_hash| 
-    if key_hash[:clearance] == true 
-      key_hash[:price] = (key_hash[:price] *
-      0.8).round(2) 
-    end 
-  end 
-cart 
 end
 
 
@@ -48,13 +16,4 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  sum = 0 
-  new_cart = consolidate_cart(cart) 
-  coupon_cart = apply_coupons(new_cart, coupons) 
-  clearance_cart = apply_clearance(coupon_cart) 
-  clearance_cart.each do |item, key_hash| 
-    sum += (key_hash[:price] * key_hash[:count])
-  end 
-sum = (sum * 0.9) if sum > 100 sum 
-end
-
+ 
