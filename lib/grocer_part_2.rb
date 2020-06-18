b2968ffea65f638d16af7fb5859e069e56248a5f
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-18 16:50:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -28,9 +28,13 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  
-  
-  
+  counter = 0
+  while counter < cart.length
+    if cart[counter][:clearance]
+      cart[counter][:price] = cart[counter][:price] = (cart[counter][:price] * 0.2)
+    end
+  counter += 1
+end
 end
 
 
